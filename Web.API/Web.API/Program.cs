fadd89f20f3b3c041c9dc92c881dd8c1aa412743
TrieuNguyen <tnguyen150@slb.com>
2025-08-18 23:09:45 +0700

    add swagger

--- Web.API/Web.API/Program.cs
+++ Web.API/Web.API/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
